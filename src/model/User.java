00ecaaeb7dc9deb071d34d1411f75d27bfefdf23
AnswerBJTU <18610946426@163.com>
2014-07-08 10:49:39 +0800

    First Project

---
+++ src/model/User.java
@@ -0,0 +1,26 @@
+package model;
+
+public class User {
+	private String userName;
+	private String userID;
+	private String userPassword;
+	
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getUserID() {
+		return userID;
+	}
+	public void setUserID(String userID) {
+		this.userID = userID;
+	}
+	public String getUserPassword() {
+		return userPassword;
+	}
+	public void setUserPassword(String userPassword) {
+		this.userPassword = userPassword;
+	}
+}
